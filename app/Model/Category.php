a00c4b5d7bfb9b5b4593a9399326ede004acb130
Chuong Nguyen <chuong.nguyenquoc007@mor.com.vn>
2022-03-04 17:35:21 +0700

    validation

--- app/Model/Category.php
+++ app/Model/Category.php
@@ -2,4 +2,22 @@
 class Category extends AppModel {
     var $name='Category';
     public $actsAs = array('Tree');
+    public $validate = array(
+        'name' =>array(
+            'not empty' => array(
+                'rule' => 'notBlank',
+                'message' => 'Name is required'
+            )
+        ),
+        'id' => array(
+            'Match Id' => array(
+                'rule' => 'matchId',
+                'message' => 'ID match failed'
+            ),
+            'not empty' => array(
+                'rule' => 'notBlank',
+                'message' => 'ID is required'
+            )
+        )
+    );
 }
